78aa64a896ca277990b753459cf9fb2ea9768f42
NicolasPozzer <pozzernico@gmail.com>
2023-12-28 09:58:52 -0300

    publish in git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GuiComponent } from './components/gui/gui.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GuiComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/gui/gui.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gui',
+  templateUrl: './gui.component.html',
+  styleUrl: './gui.component.css'
+})
+export class GuiComponent {
+
+}
